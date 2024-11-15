cec2f64b831d8e991377e67ca83d66cbd480638f
Manhkientruc <caomanh1809@gmail.com>
2024-11-15 22:15:30 +0700

    update

---
+++ app/src/main/java/com/pro/movie/activity/PlayMovieActivity.java
@@ -0,0 +1,226 @@
+package com.pro.movie.activity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.pro.movie.MyApplication;
+import com.pro.movie.R;
+import com.pro.movie.model.Movie;
+import com.pro.movie.model.UserInfor;
+import com.pro.movie.prefs.DataStoreManager;
+import com.pro.movie.utils.GlobalFunction;
+
+public class PlayMovieActivity extends BaseActivity {
+
+    private MaterialDialog progressDialog;
+    private WebView webView;
+    private int mMovieId;
+    private Movie mMovie;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_play_movie);
+
+        getDataIntent();
+        initUi();
+        getMovieDetail();
+    }
+
+    private void getDataIntent() {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle == null) {
+            return;
+        }
+        mMovieId = bundle.getInt("movie_id");
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initUi() {
+        webView = findViewById(R.id.web_view);
+
+        progressDialog = new MaterialDialog.Builder(this)
+                .content(R.string.waiting_message)
+                .progress(true, 0)
+                .build();
+        progressDialog.show();
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setSupportZoom(false);
+        webSettings.setSaveFormData(true);
+        webSettings.setDomStorageEnabled(true);
+        //improve webView performance
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebChromeClient(new ChromeClient());
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                setTitle("Loading " + mMovie.getTitle());
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                setTitle(view.getTitle());
+                if (progressDialog != null) progressDialog.dismiss();
+            }
+        });
+
+        LinearLayout layoutFooter = findViewById(R.id.layout_footer);
+        layoutFooter.setOnClickListener(view -> onBackPressed());
+    }
+
+    private void getMovieDetail() {
+        MyApplication.get(this).getMovieDetailDatabaseReference(mMovieId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mMovie = snapshot.getValue(Movie.class);
+                if (mMovie != null) {
+                    webView.loadUrl(mMovie.getUrl());
+                    addHistory();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(PlayMovieActivity.this,
+                        getString(R.string.msg_get_date_error), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addHistory() {
+        if (mMovie == null || GlobalFunction.isHistory(mMovie)) {
+            return;
+        }
+        String userEmail = DataStoreManager.getUser().getEmail();
+        UserInfor userInfor = new UserInfor(System.currentTimeMillis(), userEmail);
+        MyApplication.get(this).getMovieDatabaseReference()
+                .child(String.valueOf(mMovie.getId()))
+                .child("history")
+                .child(String.valueOf(userInfor.getId()))
+                .setValue(userInfor);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        webView.restoreState(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        webView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.onResume();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    finish();
+                }
+                return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class ChromeClient extends WebChromeClient {
+        private View mCustomView;
+        private CustomViewCallback mCustomViewCallback;
+        private int mOriginalOrientation;
+        private int mOriginalSystemUiVisibility;
+
+        ChromeClient() {
+        }
+
+        public Bitmap getDefaultVideoPoster() {
+            if (mCustomView == null) {
+                return null;
+            }
+            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+        }
+
+        public void onHideCustomView() {
+            ((FrameLayout) getWindow().getDecorView()).removeView(this.mCustomView);
+            this.mCustomView = null;
+            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+            setRequestedOrientation(this.mOriginalOrientation);
+            this.mCustomViewCallback.onCustomViewHidden();
+            this.mCustomViewCallback = null;
+        }
+
+        public void onShowCustomView(View paramView, CustomViewCallback paramCustomViewCallback) {
+            if (this.mCustomView != null) {
+                onHideCustomView();
+                return;
+            }
+            this.mCustomView = paramView;
+            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+            this.mOriginalOrientation = getRequestedOrientation();
+            this.mCustomViewCallback = paramCustomViewCallback;
+            ((FrameLayout) getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+            getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+}
